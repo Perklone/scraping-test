8f412c315f67b9836b488d14edb4be9d5b1e632b
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2025-08-14 23:00:53 +0700

    chore: add example runner for easier testing

--- ruby.rb
+++ ruby.rb
@@ -1,7 +1,20 @@
 require_relative "results/extractor"
 
+# van-gogh-paintings.html
+test = Extractor.new.extract("files/van-gogh-paintings.html")
+File.open("output/van-gogh.json", "w") do |file|
+  file.puts JSON.pretty_generate(test)
+end
+
+# monet-paintings.html
 test = Extractor.new.extract("files/monet-paintings.html")
-File.open("monet.json", "w") do |file|
+File.open("output/monet.json", "w") do |file|
+  file.puts JSON.pretty_generate(test)
+end
+
+# van-gogh-paintings.html
+test = Extractor.new.extract("files/dog-breed-list.html")
+File.open("output/dog-breed.json", "w") do |file|
   file.puts JSON.pretty_generate(test)
 end
 
