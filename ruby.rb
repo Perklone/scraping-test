34c6e86bd418f8249c9edc3cd50d24e4ac940b50
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2025-08-14 21:36:08 +0700

    chore: use proper formatting

--- ruby.rb
+++ ruby.rb
@@ -7,6 +7,7 @@ def extract(html_file)
 
   carousel = html_doc.css("a").select do |link|
     # This is some assumption that the carousel is an <a> tag with an image, and a url that comes with sca_esv
+    # Based on observation, this is quite different from the "People also search for" section where the <a> tag is nested with <div> below it.
     link.css("img").any? && link[:href]&.include?('sca_esv')
   end
 
@@ -14,16 +15,28 @@ def extract(html_file)
 
   carousel.each do |object|
     name = object.css("img").first[:alt] # since it's an array
-
     date_div = object.css('div').find { |div| div.text.match(/^\d{4}$/) }
-    date = date_div ? date_div.text : nil
     link = "https://www.google.com" + object[:href]
+
+=begin
+Alternatively, You can do something like
+name = object.css.('div')[1]
+date = object.css.('div')[2]
+
+but this is brittle and prune to break if the order changes.
+But the method that I used also can be prone to mistake (if the alt is not properly named.)
+=end
 
     res = {
       name: name,
-      date: [date],
       link: link,
     }
+
+    date = date_div ? date_div.text : nil
+    res[:extensions] = [date] if !date.nil?
+
+    image = object.css("img").first["data-src"]
+    res[:image] = image if !image.nil?
 
     result[:artworks] << res
   end
